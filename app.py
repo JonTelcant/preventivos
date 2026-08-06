4dc58c3b08764a96b656b0de048a026696f0d922
JonTelcant <jonlopez@telcant.es>
2026-08-06 13:25:14 +0200

    sd

--- app.py
+++ app.py
@@ -603,12 +603,12 @@ def rellenar():
     try:
         ruta_local = MAPA_FILE  # por ejemplo 'mapa_preventivos.xlsx'
         descargar_archivo_r2('mapa_preventivos.xlsx', 'preventivos', ruta_local)
-        wb = openpyxl.load_workbook(ruta_local)
+        
     except Exception as e:
         return f"CRASH DETECTADO: {str(e)}"
         #flash(f'Error al cargar el archivo mapa_preventivos.xlsx: {str(e)}')
         #return redirect(url_for('index'))
-    
+    wb = openpyxl.load_workbook(ruta_local)
     # Para cada tipo, buscar la hoja correspondiente y leer las preguntas
     items_por_tipo = {}  # Para mantener el orden original
     equipos_por_tipo = {}  # Para guardar información de equipos por tipo
