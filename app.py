b6920d8256ce2adb806a2a7a2080aafdc2d09364
JonTelcant <jonlopez@telcant.es>
2026-08-08 13:02:12 +0200

    g v

--- app.py
+++ app.py
@@ -580,7 +580,7 @@ def guardar_configuracion_mapa(tipo_preventivo, configuraciones, info_especial):
         wb.close()
         ruta_temporal = os.path.join(DOWNLOAD_FOLDER, "mapa_preventivos.xlsx")
         carpeta_destino = "preventivos"
-        exito_r2, mensaje_r2 = subir_archivo_r2(ruta_temporal, "mapa_preventivos.xlsx", carpeta_destino)
+        exito_r2, mensaje_r2 = subir_archivo_r2(ruta_temporal, 'mapa_preventivos.xlsx', carpeta_destino)
         # Limpiar archivo temporal
         os.remove(ruta_temporal)
         return True, None
